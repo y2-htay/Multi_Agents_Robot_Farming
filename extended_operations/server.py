ba73f7a7d30ce0e777a9a0938654590c9c22a2c9
sweet <Yamin2.Htay@live.uwe.ac.uk>
2024-12-03 20:02:42 +0000

    Extended oprations files added /

---
+++ extended_operations/server.py
@@ -0,0 +1,32 @@
+# from mesa.visualization.ModularVisualization import ModularServer
+# from mesa.visualization.modules import CanvasGrid
+# from model import FarmModel
+# from portrayal import farm_portrayal
+
+import warnings
+warnings.filterwarnings("ignore", category  = FutureWarning)
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid
+from model import FarmModel
+from portrayal import farm_portrayal
+
+# Set up the grid visualization
+print("Setting up CanvasGrid....")      # debug
+canvas_element = CanvasGrid(farm_portrayal, 29, 25, 609, 600)
+
+
+print(f"Initialising the ModularServer....")  #debug
+# Initialize the server
+server = ModularServer(
+    FarmModel,  # Model class
+    [canvas_element],  # Visualization elements
+    "Farm Simulation",  # Title of the simulation
+    {"width": 29, "height": 25, "num_robots": 2},  # Model parameters
+)
+
+#debug
+print("Server is ready.")
+
+
+
+
